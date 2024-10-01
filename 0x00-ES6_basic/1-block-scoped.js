afb176fb27c03371512651756f7be87915ae3ea8
ondieki1237 <makoriseth1237@gmail.com>
2024-10-01 04:53:13 +0300

    makori

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,13 @@
-export function taskBlock(trueOrFalse)
-{
-    const task = false;
-    const task2 = true;
-    if (trueOrFalse)
-    {
-        let task = true;
-        let task2 = false;
-        console.log(task, task2);
-    }
-    return [task, task2];
-}
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
 
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+    console.log(task, task2);
+  }
+
+  return [task, task2];
+}
 
--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -1,1 +1,5 @@
 import taskBlock from './1-block-scoped.js';
+
+console.log(taskBlock(true));
+console.log(taskBlock(false));
+
