fbb5fb9edb6eacf4705ab7c063f360661c017fe2
ondieki1237 <makoriseth1237@gmail.com>
2024-10-01 04:29:24 +0300

    task BLOCK

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+export function taskBlock(trueOrFalse)
+{
+    const task = false;
+    const task2 = true;
+    if (trueOrFalse)
+    {
+        let task = true;
+        let task2 = false;
+        console.log(task, task2);
+    }
+    return [task, task2];
+}
+
+
