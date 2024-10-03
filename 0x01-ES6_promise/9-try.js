21197595f7168926f3c9e3dd1c0fa5b45e12b668
ondieki1237 <makoriseth1237@gmail.com>
2024-10-03 02:17:44 +0000

    makori

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+// 9-try.js
+function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
+
+export default guardrail;
